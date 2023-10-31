add7ae91be05b8281b4daaee2e27d2a28faed876
Eric Hannus <hannus.eric@gmail.com>
2023-10-31 11:53:39 +0200

    Cleaned up first constrained DMP

--- TC_DMP_constrainedVelAcc/own_main.m
+++ TC_DMP_constrainedVelAcc/own_main.m
@@ -2,36 +2,19 @@ clearvars
 clc
 close all 
 
-addpath('/home/erichannus/catkin_ws/src/MatlabPreProcess/')
+addpath('/home/erichannus/catkin_ws/src/SupportScripts/')
 addpath('/home/erichannus/catkin_ws/src/Data/demos/')
 
 
 % Create demonstration trajectory
-%filename = 'sack_from_bag2.csv';
-%D = preprocess(filename, false, 0.57, 0, -0.20, 1); %other processing of x distance to avoid collisions... >>  if larger range of x allowed then robots need to be moved closer potentially
-
 filename = '10l_bag_flip.csv';
 D = preprocess(filename, false, 0.00, 0.00, 0.00, 1, 'ori2', 0.38);
-%D = preprocess(filename, false, 0.05, -0.05, 0.05, 1);
-
-Dsmooth = smoothdata(D, 1, "gaussian",35); %still noisy after IK
+Dsmooth = smoothdata(D, 1, "gaussian",35); %smooth demo before calculating IK
 Dsmooth(:,4:7) = Dsmooth(:,4:7) ./ sqrt(sum(Dsmooth(:,4:7).^2,2)); %Make sure quaternion is still unit
-
 [q, jacobians] = InverseKinematics(Dsmooth);
 
-poseIKFK = ForwardKinematics(q);
-
-%q = smoothdata(q, 2, "gaussian",10);
-
-
-
 %generate demo struct
-delta_t = 1/120;
-demo_traj.t = delta_t * [0:1:length(q')-1];
-demo_traj.pos = q';
-demo_traj.vel = [diff(q',1,2)/delta_t zeros(size(demo_traj.pos,1), 1)];
-demo_traj.acc = [diff(demo_traj.vel,1,2)/delta_t zeros(size(demo_traj.pos,1), 1)];
-
+demo_traj = generateDemo(q', 1/120);
 
 % Nominal trajectory functions
 dmp_params.D = 20;
@@ -44,102 +27,71 @@ dmp_params.alpha_s = 1;
 dmp = DMP(dmp_params,demo_traj);
 
 % Simulation setup
-sim_params.dt = 1/1000; %NOTE: this is actually correct rate for Frankas too!
-%sim_params.dt = 1/120; %Test with 1/120 to fit DMP with OptiTrack frequency
-sim_params.T_max = 100;
-%sim_params.kv = 10;
-%sim_params.kp = sim_params.kv^2/4;
+sim_params.dt = 1/1000; %frequency of Franka robots
+sim_params.T_max = 100; %maximum duration after slowdown due to constraints - original value 100s is much longer than demonstrated trajectory lengths so this value is fine
 
-nominalTraj = dmp.rollout(sim_params.dt); %ROLLOUT only for nominal traj? Then use DMP.step in simulation script to get constrained DMP result?
-%sim_params.a_max = 0.5*[max(abs(nominalTraj.acc(1,:))); max(abs(nominalTraj.acc(2,:)))];
-%sim_params.v_max = inf*[1 1]'; %NOTE: change this if I want to have v limit from the start
-%sim_params.v_max = [0.15 0.15]';
-%sim_params.v_max = 0.5*[max(abs(nominalTraj.vel(1,:))); max(abs(nominalTraj.vel(2,:)))];
-%sim_params.s_v_max_online = 0.65;
-%sim_params.v_max_online = [inf;0.15]; %leave defined here for plotting, but disable in sim
-% sim_params.v_max_online = inf*[1 1]'; %Commented away in original code,
-% can be uncommented to remove limit
+%Unconstrained DMP
+nominalTraj = dmp.rollout(sim_params.dt);
+writematrix(nominalTraj.pos',fullfile('/home/erichannus/catkin_ws/src/Data/trajectories',strcat('nominalDMP2_joint_',filename)))
+writematrix(nominalTraj.vel',fullfile('/home/erichannus/catkin_ws/src/Data/trajectories',strcat('nominalDMP2_joint_vel_',filename)))
 
 
-%WORST CASE COMMON LIMITS
-p_max = [2.7437; 1.7628; 2.8973; -0.1518; 2.8065; 3.7525; 2.8973]; %not used in sim, added for PLOTTING
-p_min = [-2.7437; -1.7628; -2.8973; -3.0421; -2.8065; 0.5445; -2.8973]; %not used in sim, added for PLOTTING
+%limits are chosen to be worst case between Franka Panda and Franka
+%Research 3
+p_max = [2.7437; 1.7628; 2.8973; -0.1518; 2.8065; 3.7525; 2.8973]; %not used in sim, added for plotting
+p_min = [-2.7437; -1.7628; -2.8973; -3.0421; -2.8065; 0.5445; -2.8973]; %not used in sim, added for plotting
 sim_params.v_max = [2.1750; 2.1750; 2.1750; 2.1750; 2.6100; 2.6100; 2.6100];
 sim_params.a_max = [10; 7.5; 10; 10; 10; 10; 10];
-%NOTE: scale vel and acc limits by 0.98 in run_simulation.m
+%note: vel and acc limits are scaled by 0.98 when passed to own_run_simulation.m for
+%additional margin
 
 % Scaling parameters
 tc_params.nominal_tau = dmp.nominal_tau;
 tc_params.eps = 1e-3;
 tc_params.gamma_nominal = 1;
 tc_params.gamma_a = 0.5; %originally 0.5, higher should reducde overshoots but increase runtime
-tc_params.a_max = sim_params.a_max * 0.98; %NOTE added scaling here to have some margin as vel/acc limits are not guaranteed
-tc_params.v_max = sim_params.v_max * 0.98; %NOTE added scaling here to have some margin as vel/acc limits are not guaranteed
-%alternativley gamma_a can be increased to get rid of small overshoots, but
-%trajectory slowdown experimentally seems to be lower when adding small
-%margin by scaling 0.98
+tc_params.a_max = sim_params.a_max * 0.98; %added scaling here to have some margin as acc limits are not guaranteed
+tc_params.v_max = sim_params.v_max * 0.98; %use same scaling for vel limits as for acc
 
-% Create temporal coupling objects
-tc = {};
-tc{1} = TemporalCoupling(tc_params);
-%tc{2} = NoTemporalCoupling(tc_params);
-%tc{3} = TemporalCouplingNoPotentials(tc_params);
+% Create temporal coupling object
+tc = TemporalCoupling(tc_params);
 
-%TODO: clean up this + plotting, no need to use cells when only tc{1} is
-%used
 % Simulate
-nSim = length(tc);
-res = cell(length(tc),1);
-for d = 1:nSim
-    disp(['Running simulation ' num2str(d) '/' num2str(length(tc)) '...']);
-    res{d} = own_run_simulation(dmp,tc{d},sim_params);   
-end
+res = own_run_simulation(dmp,tc,sim_params);   
 
-%plot whether limits are exceeded
-min_joint_pos = (min(res{1}.ref_pos, [], 2) < p_min)'
-max_joint_pos = (max(res{1}.ref_pos, [], 2) > p_max)'
+%print whether limits are exceeded
+min_joint_pos = (min(res.ref_pos, [], 2) < p_min)'
+max_joint_pos = (max(res.ref_pos, [], 2) > p_max)'
 
-min_joint_vel = (min(res{1}.ref_vel, [], 2) < -sim_params.v_max)' 
-max_joint_vel = (max(res{1}.ref_vel, [], 2) > sim_params.v_max)'
+min_joint_vel = (min(res.ref_vel, [], 2) < -sim_params.v_max)' 
+max_joint_vel = (max(res.ref_vel, [], 2) > sim_params.v_max)'
 
-min_joint_acc = (min(res{1}.ref_acc, [], 2) < -sim_params.a_max)'
-max_joint_acc = (max(res{1}.ref_acc, [], 2) > sim_params.a_max)'
+min_joint_acc = (min(res.ref_acc, [], 2) < -sim_params.a_max)'
+max_joint_acc = (max(res.ref_acc, [], 2) > sim_params.a_max)'
 
-slowdown = res{1}.t(end) - demo_traj.t(end)
+slowdown = res.t(end) - demo_traj.t(end) %how much longer runtime (in seconds) does the constrained DMP have compared to the demonstrated trajectory
 
-%first do this for plotting
-poseDMP = ForwardKinematics(res{1}.ref_pos');
+%calculate the corresponding cartesian trajectory for plotting
+poseDMP = ForwardKinematics(res.ref_pos');
 
 % Plot result
 own_plot_result
 
-%also do this for both robots and let controller do sign flips
-%NOTE: later change controller to use Franka3 as default and these values
-%would not need to be flipped? Note jonint7 is not just a flip but also
-%some offset (see controller code)
-res{1}.ref_pos(1,:) = -res{1}.ref_pos(1,:);
-res{1}.ref_pos(3,:) = -res{1}.ref_pos(3,:);
-res{1}.ref_pos(5,:) = -res{1}.ref_pos(5,:);
-res{1}.ref_pos(7,:) = -res{1}.ref_pos(7,:);
+%Flip these joint signs so that output DMP runs on Franka2 in the lab
+%wihtout sign flips, and flip signs back for Franka3 in the controller code
+res.ref_pos(1,:) = -res.ref_pos(1,:);
+res.ref_pos(3,:) = -res.ref_pos(3,:);
+res.ref_pos(5,:) = -res.ref_pos(5,:);
+res.ref_pos(7,:) = -res.ref_pos(7,:);
+writematrix(res.ref_pos',fullfile('/home/erichannus/catkin_ws/src/Data/trajectories',strcat('DMP2_joint_',filename)))
 
-%Run Forward Kinematics for plotting and playback with cartesian control
-%previously and do this again so that pose with flipped joints is saved for pose
-%trajectory
-poseDMP = ForwardKinematics(res{1}.ref_pos');
+res.ref_vel(1,:) = -res.ref_vel(1,:);
+res.ref_vel(3,:) = -res.ref_vel(3,:);
+res.ref_vel(5,:) = -res.ref_vel(5,:);
+res.ref_vel(7,:) = -res.ref_vel(7,:);
+writematrix(res.ref_vel',fullfile('/home/erichannus/catkin_ws/src/Data/trajectories',strcat('DMP2_joint_vel_',filename)))
 
-%save joint angles to file
-%writematrix(res{1}.ref_pos',fullfile('/home/erichannus/catkin_ws/src/Data/trajectories',strcat('joint_',filename)))
-%writematrix(poseDMP,fullfile('/home/erichannus/catkin_ws/src/Data/trajectories',strcat('pose_',filename)))
-
-%use general filename for to easily switch between demos when generating
-%from new files above
-writematrix(res{1}.ref_pos',fullfile('/home/erichannus/catkin_ws/src/Data/trajectories','joint_demoDMP.csv'))
-writematrix(poseDMP,fullfile('/home/erichannus/catkin_ws/src/Data/trajectories','pose_demoDMP.csv'))
-
-res{1}.ref_vel(1,:) = -res{1}.ref_vel(1,:);
-res{1}.ref_vel(3,:) = -res{1}.ref_vel(3,:);
-res{1}.ref_vel(5,:) = -res{1}.ref_vel(5,:);
-res{1}.ref_vel(7,:) = -res{1}.ref_vel(7,:);
-
-%writematrix(res{1}.ref_vel',fullfile('/home/erichannus/catkin_ws/src/Data/trajectories',strcat('joint_vel_',filename)))
-writematrix(res{1}.ref_vel',fullfile('/home/erichannus/catkin_ws/src/Data/trajectories','joint_vel_demoDMP.csv'))
+%run forward kinematics again after sign flips so that cartesian trajectory
+%can be plotted for comparison in control scripts
+poseDMP = ForwardKinematics(res.ref_pos');
+writematrix(poseDMP,fullfile('/home/erichannus/catkin_ws/src/Data/trajectories',strcat('DMP2_pose_',filename)))
--- TC_DMP_constrainedVelAcc/own_run_simulation.m
+++ TC_DMP_constrainedVelAcc/own_run_simulation.m
@@ -3,12 +3,6 @@ function res = run_simulation(dmp,tmp_couple,params)
 % Parameters
 dt = params.dt;
 T_max = params.T_max;
-%kp = params.kp;
-%kv = params.kv;
-%a_max = params.a_max;
-%v_max = params.v_max;
-%s_v_max_online = params.s_v_max_online;
-%v_max_online = params.v_max_online;
 
 % Initialize
 dmp = dmp.init();
@@ -40,54 +34,13 @@ while t < T_max
     k = k + 1;
     t = t + dt;
     
-    %{
-    % Add velocity limit online
-    if dmp.s() < s_v_max_online
-        v_max = v_max_online;
-        tmp_couple.v_max= v_max_online;
-    end
-    %}
-
     % Step trajectory generator
     tau_dot = tmp_couple.tau_dot(dmp,dt);
     dmp = dmp.step(tau_dot,dt); 
 
-    % Controller
-
-    %NOTE: acceleration control with kp and kv ++ saturate a and v within
-    %limits!! >> rewrite to just run reference DMP as is??
-    
-    %Possibility of using saturated controller for Franka? 
-    %See rate limits here: https://frankaemika.github.io/docs/libfranka.html
-    
-    %{
-    sys_acc_ctrl = dmp.ref_acc() + kv*(dmp.ref_vel()-sys.vel) + kp*(dmp.ref_pos()-sys.pos); %NOTE: DMP ref acc/vel/pos were updated in dmp.step() above!
-    % Saturate acceleration
-    sys_acc_ctrl = min(max(sys_acc_ctrl,-a_max),a_max);
-    % Velocity integration
-    vel_prev = sys.vel;
-    sys.vel = sys.vel + sys_acc_ctrl*dt;
-    % Saturate velocity
-    sys.vel = min(max(sys.vel,-v_max),v_max);
-    sys.acc = (sys.vel-vel_prev)/dt;
-    % Position integration
-    sys.pos = sys.pos + sys.vel*dt;
-    %}
-    
     %Direcly using DMP reference as system output
     sys.acc = dmp.ref_acc();
     sys.vel = dmp.ref_vel();
     sys.pos = dmp.ref_pos();
-    %Now the system is not actually limited by vel and acc (no saturation)
-    %so it looks like unconstrained DMP can follow motion (while breaking
-    %vel and acc constraints) from plots too... >> this should be OK as we
-    %can see from plots that constrained DMP now follows path while keeping
-    %vel and acc wihtin bounds 
-
-    %%NEXT: try on own reference trajs + 7 DOF (joints)
-        %first some 2 DOF so plot works, just manage how to create struct
-        %from csv
-        %then all 7 joints + corresponding plots!
-
 
 end
